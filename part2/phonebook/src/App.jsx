2981338d8be97670cd1f8eab1c745f447365a7af
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2024-12-12 16:07:01 +0200

    exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,23 +2,32 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
+
+    const newPerson = { name: newName, number: newNumber }
+
+    if (!newPerson.name) {
+      alert('Please give a name')
+      return
+    }
     if (persons.some(person => person.name === newPerson.name)) {
       alert(`${newPerson.name} is already added to the phonebook`)
       return
     }
+
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   const Person = ({ person }) => {
-    return <li>{person.name}</li>
+    return <li>{`${person.name} ${person.number}`}</li>
   }
 
   return (
@@ -27,6 +36,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={(event) => { setNewName(event.target.value) }} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(event) => { setNewNumber(event.target.value) }} />
         </div>
         <div>
           <button type="submit">add</button>
