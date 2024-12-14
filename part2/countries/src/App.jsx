dc3a15311b4e2174f5b4dd4da5ca8bde6409eac3
Sampo Riitala <57003465+realtalin@users.noreply.github.com>
2024-12-14 23:26:57 +0200

    exercise 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
+const OPENWEATHER_KEY = import.meta.env.VITE_OPENWEATHER_KEY
+
 const Search = ({ search, handleSearch }) => {
   return (
     <div>
@@ -55,7 +57,7 @@ const CountryList = ({ countries, setDisplayedCountry }) => {
   }
 }
 
-const CountryInfo = ({ country }) => {
+const CountryInfo = ({ country, weather }) => {
   if (!country) {
     return null
   }
@@ -66,7 +68,7 @@ const CountryInfo = ({ country }) => {
         <li>capital {country.capital[0]}</li>
         <li>area {country.area}</li>
       </ul>
-      <h2 style={{ padding: 10 }}>languages:</h2>
+      <h3 style={{ padding: 10 }}>languages:</h3>
       <ul>
         {Object.entries(country.languages).map((language) => (
           <li key={language[0]}>{language[1]}</li>
@@ -76,6 +78,24 @@ const CountryInfo = ({ country }) => {
         src={country.flags.svg}
         style={{ width: '15em', height: 'auto' }}
       ></img>
+      <h2>Weather in {country.capital[0]}</h2>
+      <ul style={{ listStyle: 'none', paddingLeft: 10 }}>
+        {weather ? (
+          <>
+            <li>
+              {`temperature ${(weather.main.temp - 273.15).toFixed(2)} Celsius`}
+            </li>
+            <li>
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              />
+            </li>
+            <li>{`wind ${weather.wind.speed} m/s`}</li>
+          </>
+        ) : (
+          <li>No weather data</li>
+        )}
+      </ul>
     </div>
   )
 }
@@ -85,12 +105,31 @@ const App = () => {
   const [allCountries, setAllCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
   const [displayedCountry, setDisplayedCountry] = useState(null)
+  const [displayedCountryWeather, setDisplayedCountryWeather] = useState(null)
 
   useEffect(() => {
     axios
       .get('https://studies.cs.helsinki.fi/restcountries/api/all')
       .then((response) => setAllCountries(response.data))
   }, [])
+
+  useEffect(() => {
+    if (displayedCountry) {
+      axios
+        .get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${displayedCountry.capital[0]}&limit=1&appid=${OPENWEATHER_KEY}`
+        )
+        .then((response) => {
+          axios
+            .get(
+              `http://api.openweathermap.org/data/2.5/weather?lat=${response.data[0].lat}&lon=${response.data[0].lon}&appid=${OPENWEATHER_KEY}`
+            )
+            .then((response) => {
+              setDisplayedCountryWeather(response.data)
+            })
+        })
+    }
+  }, [displayedCountry])
 
   const handleSearch = (event) => {
     const searchValue = event.target.value
@@ -112,7 +151,10 @@ const App = () => {
         displayedCountry={displayedCountry}
         setDisplayedCountry={setDisplayedCountry}
       />
-      <CountryInfo country={displayedCountry} />
+      <CountryInfo
+        country={displayedCountry}
+        weather={displayedCountryWeather}
+      />
     </div>
   )
 }
