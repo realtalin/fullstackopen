de2ebeb704608cba228d9d4b4275b185097682c0
Sampo <57003465+realtalin@users.noreply.github.com>
2024-12-29 18:32:34 +0200

    exercise 5.10

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -68,9 +68,11 @@ const BlogList = ({ blogs, updateBlog }) => {
   return (
     <>
       <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      ))}
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+        ))}
     </>
   )
 }
