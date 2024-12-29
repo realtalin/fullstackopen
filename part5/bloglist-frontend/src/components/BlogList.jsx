5650c3559139f1bf9d54a7420995854c2bf18488
Sampo <57003465+realtalin@users.noreply.github.com>
2024-12-29 17:36:54 +0200

    exercise 5.7

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -1,8 +1,56 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+  const divStyle = {
+    paddingTop: 5,
+    paddingBottom: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 10,
+  }
+  const listStyle = {
+    listStyleType: 'none',
+  }
+
+  const toggleDetailsVisibility = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  return (
+    <div>
+      {detailsVisible ? (
+        <div style={divStyle}>
+          <ul style={listStyle}>
+            <li>
+              {blog.title} {blog.author}
+              <button onClick={toggleDetailsVisibility}>hide</button>
+            </li>
+            <li>
+              <p>{blog.url}</p>
+            </li>
+            <li>
+              {blog.likes}
+              <button>like</button>
+            </li>
+            <li>
+              <p>{blog.user.name}</p>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div style={divStyle}>
+          <ul style={listStyle}>
+            <li>
+              {blog.title} {blog.author}
+              <button onClick={toggleDetailsVisibility}>show</button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}
 
 const BlogList = ({ blogs }) => {
   return (
